e3a80811b9baa4907fdf06de360f4292bd61f081
Khachatur Khalatyan <xalatyanxachatur@yahoo.com>
2017-08-05 02:13:47 +0400

    Add prompt

---
+++ calc.js
@@ -0,0 +1,13 @@
+function f(digit){
+  var b = parseInt(digit/2);
+  var c = digit%2;
+  digit = b;
+  var res = "" + c;
+  if(b < 1){
+    return  1;
+  }
+ 
+  return  Number(f(digit) + res);
+}
+
+console.log(f(5));
